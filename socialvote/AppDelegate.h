26b4867173dc81887ba5c67185484cf745886c5a
edwardyangey <wangshuai@wangs-MacBook-Pro.local>
2013-09-10 02:19:20 +0100

    first commit

---
+++ socialvote/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  socialvote
+//
+//  Created by wang shuai on 10/09/2013.
+//  Copyright (c) 2013 streamsdk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
